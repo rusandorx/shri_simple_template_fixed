a5fb0dc52fc32b4f2490a134a8d0f4d3c18f3850
Rusandor <ruslanpalkin44@gmail.com>
2022-10-23 23:01:02 +0300

    Test pr 1

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ export function About() {
   return (
     <>
       <h1 data-testid="page-title">About</h1>
-      <p>This is an example application. Test 1</p>
+      <p>This is an example application. Test 1 </p>
     </>
   );
 }

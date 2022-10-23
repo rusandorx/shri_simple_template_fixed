652a9f284f34837a303d3531dfdb6b09849666a5
Rusandor <ruslanpalkin44@gmail.com>
2022-10-24 00:26:27 +0300

    THE WORST BUG HAS BEEN FIXED

--- src/store.js
+++ src/store.js
@@ -10,7 +10,8 @@ export const setText = (text) => ({ type: 'SET_TEXT', text });
 export const setDone = (index, done) => ({ type: 'SET_DONE', index, done });
 
 // reducer
-const rootReducer = (action, state = DEFAULT_STATE) => {
+// eslint-disable-next-line default-param-last
+const rootReducer = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
     case 'ADD_ITEM':
       return {
